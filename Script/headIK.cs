8587b5c4e5d0305ce62866bc87654c224cf76142
LUK3D <31851972+LUK3D@users.noreply.github.com>
2019-09-08 10:26:44 +0100

    Script e Guia de uso
    O vídeo demo pode ser encontrado aqui: https://youtu.be/13a_qoU1ot8

---
+++ Script/headIK.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class headIK : MonoBehaviour
+{
+    protected Animator animator; /* Armazena o animator do personagem */
+
+    public Transform target; /* Armazena o transform do Alvo  */
+    public bool ikActive = true; /* ACTIVAR O INVERSE KINEMATIC */
+
+    void Start()
+    {
+      target = GameObject.Find("cabeca").transform; /* Objecto vasio colocado como filho do osso da cabeça do alvo */
+     animator = transform.GetComponent<Animator>(); /* Pegando o Componente Animator do Personagem com esse script */
+    }
+   
+   void OnAnimatorIK()
+    {
+        if(animator) {
+           
+            if(ikActive) {
+                if(target != null) {
+                    animator.SetLookAtWeight(1);
+                    animator.SetLookAtPosition(target.position);
+                   
+                }      
+                
+            }
+            else {          
+                
+                animator.SetLookAtWeight(0);
+            }
+        }
+    }
+}
